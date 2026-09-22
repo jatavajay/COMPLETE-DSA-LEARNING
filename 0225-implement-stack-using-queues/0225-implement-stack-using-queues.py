428c7a5f14741f3a8c293ec51c14aa95dae40e5e
Ajay Jatav <181382298+jatavajay@users.noreply.github.com>
2026-09-22 22:49:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (87.56%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,29 @@
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        self.st = deque()
+
+    def push(self, x: int) -> None:
+        self.st.append(x)
+
+        # Move all previous elements behind x
+        for _ in range(len(self.st) - 1):
+            self.st.append(self.st.popleft())
+
+    def pop(self) -> int:
+        return self.st.popleft()
+
+    def top(self) -> int:
+        return self.st[0]
+
+    def empty(self) -> bool:
+        return len(self.st) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
